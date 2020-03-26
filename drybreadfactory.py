96d95dd443742f4f6fc9c16de06a3ef454704d88
lewandowskih <hubert.1009@gmail.com>
2020-03-26 20:33:01 +0100

    swtiched to Flask framework

--- drybreadfactory.py
+++ drybreadfactory.py
@@ -1,6 +1,18 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def get_dry_bread():
-    return "Po co kotu telefon?", "Żeby MIAU"
+    return "Po co kotu telefon? Żeby MIAU"
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='localhost', port=port)
 
 
 
-print(get_dry_bread())
+
+
