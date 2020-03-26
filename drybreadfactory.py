cb285f7e0ac80bedc4b262e4781ab0c5e041e2fd
lewandowskih <hubert.1009@gmail.com>
2020-03-26 20:41:55 +0100

    changed host from localhost to 0.0.0.0

--- drybreadfactory.py
+++ drybreadfactory.py
@@ -10,7 +10,7 @@ def get_dry_bread():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='localhost', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
