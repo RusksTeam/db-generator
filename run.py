075a33529d5e1f1c1680cd536ba595332aafcc2f
lewandowskih <hubert.1009@gmail.com>
2020-03-29 18:47:53 +0200

    fix #5 - handling for mmap @ unix - heroku

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ if platform.system() == 'Windows':
     shm = mmap.mmap(0, 4, global_cfg['shared_mem']['drybread_tag'])
     shm.seek(0)
 else:#linux
-    with open(global_cfg['shared_mem']['drybread_tag'], 'wb') as f:
-        f.write(b(0))
+    with open(global_cfg['shared_mem']['drybread_tag'], 'w') as f:
+        f.write('0')
     fd = os.open(global_cfg['shared_mem']['drybread_tag'], os.O_CREAT | os.O_TRUNC | os.O_RDWR)
     shm = mmap.mmap(fd, 0, mmap.MAP_SHARED, mmap.PROT_READ | mmap.PROT_WRITE)
 
