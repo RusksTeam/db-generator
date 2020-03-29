5af1c51809c808b1d06a2dfc0b41af23a0324deb
lewandowskih <hubert.1009@gmail.com>
2020-03-29 11:50:32 +0200

    Refactored code for usage with Flask

---
+++ drybreadcfg.py
@@ -0,0 +1,4 @@
+from configparser import ConfigParser
+
+global_cfg = ConfigParser()
+global_cfg.read("config/server.ini")
--- run.py
+++ run.py
@@ -1,34 +1,14 @@
-from dbreguesthandler import DBRequestHandler
-from dbserver import DBServer
+from flask import Flask
+from  drybreadcfg import global_cfg
 import os
-import time
-import configparser
-import json
-import drybreadfactory
+app = Flask(__name__)
 
-Config = configparser.ConfigParser();
-Config.read("config/server.ini")
-HOST_NAME = Config.get('HTTPConfig', 'HOST_NAME')
-PORT_NUMBER = int(os.environ.get('PORT', Config.getint('HTTPConfig', 'PORT_NUMBER')))
+@app.route('/suchar')
+def get_dry_bread():
+    return "Po co kotu telefon?" + " " + "Żeby MIAU"
 
-
-dry_bread = drybreadfactory.get_dry_bread()
-
-#Generating json for DB response
-response = {}
-response["question"] = dry_bread[0]
-response["answer"] = dry_bread[1]
-response_string = json.dumps(response, ensure_ascii=False, indent=4)
-
-httpd = DBServer((HOST_NAME, PORT_NUMBER), DBRequestHandler)
-httpd.set_dry_bread(response_string, mime="application/json")
-#httpd.set_dry_bread('<html><head><meta content="text/html;charset=utf-8" http-equiv="Content-Type"><meta content="utf-8" http-equiv="encoding"></head><body><h1>Po co kotu telefon?</h1><h1>Żeby MIAU</h1></body></html>', mime="text/html")
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', global_cfg['server']['port_number']))
+    app.run(host=global_cfg['server']['host_name'], port=port)
 
 
-print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-httpd.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
