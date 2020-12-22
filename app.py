9989ff3d85fa7c06fc73c4bd33a8926c7a0ddaf9
Hubert Lewandowski <hubert.1009@gmail.com>
2020-12-22 16:19:34 +0100

    fetched port number from os variable

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def update_daily_drybread():
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
-    app.run(host='0.0.0.0', port='8000')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
